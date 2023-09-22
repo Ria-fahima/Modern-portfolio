718c7372c19a5be6b96e8129e36ba74bbe261272
fahimagithub <fahima96khan@gmail.com>
2023-09-23 00:34:28 +1000

    Modified the footer line

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
    <footer className='w-full border-t-2 border-solid border-dark
    font-medium text-lg'>
        <Layout className='py-8 flex items-center justify-center'>
-           <span>{new Date().getFullYear()} &copy; All Rights Reserved.</span>
+           <span>Ria{new Date().getFullYear()} &copy; All Rights Reserved.</span>
        </Layout>
    </footer>
   )
